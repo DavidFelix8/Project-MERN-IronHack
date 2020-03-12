93e516a735f4914582d6894e0f907a8b6a88dde3
David Felix <davidfelix1997@hotmail.com>
2020-03-12 21:10:53 +0000

    added services, client side, authentication in services

---
+++ client/project-mern/services/authentication.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+
+const instance = axios.create({
+  baseURL: '/api/authentication'
+});
+
+const signUp = data =>
+  new Promise((resolve, reject) => {
+    instance
+      .post('/sign-up', data)
+      .then(result => {
+        const user = result.data.user;
+        resolve(user);
+      })
+      .catch(reject);
+  });
+
+const signIn = data =>
+  new Promise((resolve, reject) => {
+    instance
+      .post('/sign-in', data)
+      .then(result => {
+        const user = result.data.user;
+        resolve(user);
+      })
+      .catch(reject);
+  });
+
+const signOut = () =>
+  new Promise((resolve, reject) => {
+    instance
+      .post('/sign-out')
+      .then(result => {
+        resolve();
+      })
+      .catch(reject);
+  });
+
+export { signIn, signUp, signOut };
