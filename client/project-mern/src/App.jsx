eeb86f8fb240e5d964f014f8732e4dba6cd52684
Pedro Meaud <meaud23@gmail.com>
2020-03-13 12:27:53 +0000

    added navbar

--- client/project-mern/src/App.jsx
+++ client/project-mern/src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <p>App </p>
+      <NavBar />
     </div>
   );
 }
