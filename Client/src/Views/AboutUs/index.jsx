d97034a29c0cc5e5894db464895c56716692ee29
Pedro Meaud <meaud23@gmail.com>
2020-03-24 11:57:14 +0000

    css changes

--- Client/src/Views/AboutUs/index.jsx
+++ Client/src/Views/AboutUs/index.jsx
@@ -8,10 +8,9 @@ function AboutUs() {
         <h1 className="About_Us-Title">Simplify. in Lisbon</h1>
         <h5 className="About_Us-Text">
           Simplify helps the residents of Lisbon focus on their priorities, and take time off from
-          managing household chores such as, getting home appliances repaired, getting the AC
-          serviced, fixing electrical issues, cleaning their house and ironing clothes. Our verified
-          and experienced services professional base ensures timely delivery of services at
-          competitive rates, while assuring customers of safety and security.
+          managing household chores. Our verified and experienced services professional base ensures
+          timely delivery of services at competitive rates, while assuring customers of safety and
+          security.
           <span className="AboutUs-Span-SubsPack">Take care of yourself, Ease the service !!!</span>
         </h5>
       </div>
