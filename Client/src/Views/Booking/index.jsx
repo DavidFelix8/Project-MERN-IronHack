33450bb7639b99a49ee41051e8541d49c7452900
David Felix <davidfelix1997@hotmail.com>
2020-03-24 11:54:06 +0000

    service list

--- Client/src/Views/Booking/index.jsx
+++ Client/src/Views/Booking/index.jsx
@@ -1,16 +1,53 @@
-import React, { Component } from 'react';
-import { list as listBooking } from './../../services/booking';
+import { Link } from 'react-router-dom';
+
+import { list as listBookings } from '../../services/payment-method';
 
 class BookingView extends Component {
-  //contructor() {
-  //super();
-  //this.state = {
+  constructor(props) {
+    super(props);
+    this.state = {
+      bookings: []
+    };
+    this.fetchData = this.fetchData.bind(this);
+  }
 
-  // }
-  //}
+  async fetchData() {
+    try {
+      const bookings = await listBookings();
+      this.setState({ bookings });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
   render() {
-    return <div></div>;
+    const bookings = this.state.bookings;
+    return (
+      <div>
+        {/* {<pre>{JSON.stringify(this.state, null, 2)}</pre>} */}
+
+        <h4>Here is your booking section</h4>
+        {bookings.map(booking => (
+          // console.log()
+          <div>
+            <span>{booking.service}</span>
+            {/*   <ol>
+          <h2>You made an appointment at:</h2>
+          <li>{booking.date}</li>
+          <h2>To the service</h2>
+          <li>{booking.service}</li>
+        </ol> */}
+          </div>
+        ))}
+        <Link to="/services" className="btn btn-primary">
+          Book another Service
+        </Link>
+      </div>
+    );
   }
 }
-
 export default BookingView;
